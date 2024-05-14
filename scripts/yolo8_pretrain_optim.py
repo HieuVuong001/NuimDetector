c6ded67dbd8179bc34dd06af31570b409db0665a
jv <jayvuong.cs@gmail.com>
2024-05-13 18:01:51 -0700

    Remove duplicated fiels

--- scripts/yolo8_pretrain_optim.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('/home/014105936/scripts/yolov8m.pt')
-
-model.train(data='/home/014105936/yolov5/nuimages.yaml', epochs=20, name='yolov8_pretrained_AdamW', batch=-1, amp=False, optimizer='AdamW')
-
-model.val()
--- scripts/yolo8_train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('/home/014105936/scripts/yolov8m.yaml')
-
-model.train(data='/home/014105936/yolov5/nuimages.yaml', epochs=20, pretrained=False, name='yolov8_yaml', batch=-1, amp=False)
-
-model.val()
